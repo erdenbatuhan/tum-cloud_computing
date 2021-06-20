f04a9ca86606601f0df910ac4048e6518232b8ed
Batuhan Erden <erden.batuhan@gmail.com>
2021-06-20 09:17:35 +0200

    refactoring

--- exercises/cc_ex3/hello-world-service/helloWorld.js
+++ exercises/cc_ex3/hello-world-service/helloWorld.js
@@ -4,7 +4,7 @@ module.exports = function (options) {
 
     // Describe the logic inside the function
     function sayWelcome(msg, respond) {
-        if(msg.name) {
+        if (msg.name) {
             var res = "Welcome " + msg.name;
             respond(null, { result: res });
         } else {
